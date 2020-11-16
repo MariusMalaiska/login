62c18af0f0fdec03e8fdcf6f87a0c3d1fc8e201d
Marius <mariusmalaiska@gmail.com>
2020-11-16 19:12:48 +0200

    works, at least

--- src/App/components/form/index.js
+++ src/App/components/form/index.js
@@ -1,60 +1,49 @@
 import React, { useState, useSelector } from "react";
-import { connect } from 'react-redux'
+
 
 import './index.css';
 
 function Form(props) {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState("");
 
-    // const dispatch = useDispatch();
-    const userLogin = useSelector(state => state.state.userValue);
 
-    const validate = () => {
-        console.log(this.userValue)
+    const callApi = e => {
 
-    }
+        if (name === "frontend@isawesome.com" || password === "cool") {
+            localStorage.setItem('token', '11111');
+        } else {
+            setErrors("User does not exist");
+        }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        validate()
+        setErrors("")
+        if (callApi()) {
+            // register(nickName, password, email);
+            console.log(name, password)
+        }
     }
 
     return (
 
         <form onSubmit={handleSubmit} className="Login--header--form" >
-            <label className="Label--username">   <input onChange={props.nameChanged} value={props.userValue} className="Input username" type="text" id="username" name="username" placeholder="Username" /></label>
-            <label className="Label--password">  <input onChange={props.passwordChanged} value={props.passwordValue} className="Input password" type="text" id="password" name="password" placeholder="Password" /></label>
+            <label className="Label--username">   <input onChange={e => setName(e.target.value)} className="Input username" type="text" id="username" name="username" placeholder="Username" /></label>
+            <label className="Label--password">  <input onChange={e => setPassword(e.target.value)} className="Input password" type="text" id="password" name="password" placeholder="Password" /></label>
+            {!!errors && <div className="Input-error">{errors}</div>}
             <button type="submit" className="Login--header--button"><span className="Login--header--text">Sign In</span></button>
             <p>{props.userValue}</p>
         </form >
     );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        userValue: state.userValue,
-        passwordValue: state.passwordValue
-    }
-}
-
-
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        nameChanged: (e) => {
-            console.log(e.target.value)
-
-            const action = { type: "INPUT_CHANGE", username: e.target.value }
-            dispatch(action);
-        },
-        passwordChanged: (e) => {
-            console.log(e.target.value)
-            const action = { type: "PASSWORD_CHANGE", password: e.target.value }
-            dispatch(action);
-        }
-    }
-}
 
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Form)
+
+
+
+export default Form
--- src/App/index.js
+++ src/App/index.js
@@ -1,27 +1,42 @@
 import './index.css';
-// import { connect } from "react-redux"
+import React, { useEffect } from "react";
 import Form from "./components/form"
 import store from "../store"
 import { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App" >
-        <div className="Login">
-          <div className="Login--header">
-            <span className="Login--header--text">Login Form</span>
-          </div>
-          <Form store={store} />
-          {/* <form action="submit" className="Login--header--form">
-          <label className="Label--username">   <input value="props.userValue" className="Input username" type="text" id="username" name="username" placeholder="Username" /></label>
-          <label className="Label--password">  <input value="props.passwordValue" className="Input password" type="text" id="password" name="password" placeholder="Password" /></label>
-          <button className="Login--header--button"><span className="Login--header--text">Sign In</span></button>
-        </form> */}
-        </div>
-      </div>
-    );
+const App = () => {
+
+
+  const getData = () => {
+    if ((localStorage.getItem('token')) === ('11111')) {
+
+      alert("you are connected!!");
+      // alert("you are connected")
+    } else {
+      console.log("no token")
+    }
   }
+
+
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+
+
+
+  return (
+    <div className="App" >
+      <div className="Login">
+        <div className="Login--header">
+          <span className="Login--header--text">Login Form</span>
+        </div>
+        <Form store={store} />
+      </div>
+    </div>
+  );
+
 }
 
 // export default connect(mapStateToProps)App;
